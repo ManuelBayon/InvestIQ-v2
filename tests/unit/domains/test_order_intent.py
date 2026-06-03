806c532ed72ecc57dde6f88978765bd792361cd8
Manuel BAYON <manuel_bayon@outlook.fr>
2026-06-03 23:52:08 +0200

    Clarify order specification naming

--- tests/unit/domains/test_order_intent.py
+++ tests/unit/domains/test_order_intent.py
@@ -1,34 +1,28 @@
 import pytest
 from math import nan
-from investiq.domain.order_intents import (
-    Side,
-    MarketOrderIntent,
-    LimitOrderIntent,
-    StopMarketOrderIntent,
-    BracketOrderIntent
-)
+from investiq.domain.order_intents import (Side, LimitOrderSpec, BracketOrderSpec, StopMarketOrderSpec, MarketOrderSpec)
 
 class TestMarketOrderIntent:
     def test_market_order_intent_raises_if_quantity_not_finite(self):
         with pytest.raises(ValueError):
-            MarketOrderIntent(
+            MarketOrderSpec(
                 quantity=nan,
                 direction=Side.BUY
             )
     def test_market_order_intent_raises_if_quantity_is_null(self):
         with pytest.raises(ValueError):
-            MarketOrderIntent(
+            MarketOrderSpec(
                 quantity=0,
                 direction=Side.BUY
             )
     def test_market_order_intent_raises_if_quantity_is_negative(self):
         with pytest.raises(ValueError):
-            MarketOrderIntent(
+            MarketOrderSpec(
                 quantity=-1,
                 direction=Side.BUY
             )
     def test_market_order_intent_valid_case(self):
-        MarketOrderIntent(
+        MarketOrderSpec(
             quantity=1,
             direction=Side.BUY
         )
@@ -36,48 +30,48 @@ class TestMarketOrderIntent:
 class TestLimitOrderIntent:
     def test_limit_order_intent_raises_if_quantity_not_finite(self):
         with pytest.raises(ValueError):
-            LimitOrderIntent(
+            LimitOrderSpec(
                 quantity=nan,
                 direction=Side.BUY,
                 price=100.0
             )
     def test_limit_order_intent_raises_if_quantity_is_null(self):
         with pytest.raises(ValueError):
-            LimitOrderIntent(
+            LimitOrderSpec(
                 quantity=0,
                 direction=Side.BUY,
                 price=100.0
             )
     def test_limit_order_intent_raises_if_quantity_is_negative(self):
         with pytest.raises(ValueError):
-            LimitOrderIntent(
+            LimitOrderSpec(
                 quantity=-1,
                 direction=Side.BUY,
                 price=100.0
             )
     def test_limit_order_intent_raises_if_price_not_finite(self):
         with pytest.raises(ValueError):
-            LimitOrderIntent(
+            LimitOrderSpec(
                 quantity=1,
                 direction=Side.BUY,
                 price=nan
             )
     def test_limit_order_intent_raises_if_price_is_null(self):
         with pytest.raises(ValueError):
-            LimitOrderIntent(
+            LimitOrderSpec(
                 quantity=1,
                 direction=Side.BUY,
                 price=0
             )
     def test_limit_order_intent_raises_if_price_is_negative(self):
         with pytest.raises(ValueError):
-            LimitOrderIntent(
+            LimitOrderSpec(
                 quantity=1,
                 direction=Side.BUY,
                 price=-1
             )
     def test_limit_order_intent_valid_case(self):
-        LimitOrderIntent(
+        LimitOrderSpec(
             quantity=1,
             direction=Side.BUY,
             price=100.0
@@ -86,35 +80,35 @@ class TestLimitOrderIntent:
 class TestStopMarketOrderIntent:
     def test_stop_market_order_intent_raises_if_trigger_price_not_finite(self):
         with pytest.raises(ValueError):
-            StopMarketOrderIntent(
+            StopMarketOrderSpec(
                 trigger_price=nan,
-                triggered_order= MarketOrderIntent(
+                triggered_order= MarketOrderSpec(
                     quantity=1,
                     direction=Side.BUY
                 )
             )
     def test_stop_market_order_intent_raises_if_trigger_price_is_null(self):
         with pytest.raises(ValueError):
-            StopMarketOrderIntent(
+            StopMarketOrderSpec(
                 trigger_price=0,
-                triggered_order= MarketOrderIntent(
+                triggered_order= MarketOrderSpec(
                     quantity=1,
                     direction=Side.BUY
                 )
             )
     def test_stop_market_order_intent_raises_if_trigger_price_is_negative(self):
         with pytest.raises(ValueError):
-            StopMarketOrderIntent(
+            StopMarketOrderSpec(
                 trigger_price=-1,
-                triggered_order= MarketOrderIntent(
+                triggered_order= MarketOrderSpec(
                     quantity=1,
                     direction=Side.BUY
                 )
             )
     def test_stop_market_order_intent_valid_case(self):
-        StopMarketOrderIntent(
+        StopMarketOrderSpec(
             trigger_price=100.0,
-            triggered_order=MarketOrderIntent(
+            triggered_order=MarketOrderSpec(
                 quantity=1,
                 direction=Side.BUY
             )
@@ -123,8 +117,8 @@ class TestStopMarketOrderIntent:
 class TestBracketOrderIntent:
     def test_bracket_order_intent_raises_if_no_target_profit_and_no_stop_loss(self):
         with pytest.raises(ValueError):
-            BracketOrderIntent(
-                entry=LimitOrderIntent(
+            BracketOrderSpec(
+                entry=LimitOrderSpec(
                     quantity=1,
                     direction=Side.BUY,
                     price=100.0
@@ -134,16 +128,16 @@ class TestBracketOrderIntent:
             )
     def test_bracket_order_intent_raises_if_stop_loss_direction_and_entry_are_same(self):
         with pytest.raises(ValueError):
-            BracketOrderIntent(
-                entry=LimitOrderIntent(
+            BracketOrderSpec(
+                entry=LimitOrderSpec(
                     quantity=1,
                     direction=Side.BUY,
                     price=100.0
                 ),
                 stop_loss=[
-                    StopMarketOrderIntent(
+                    StopMarketOrderSpec(
                         trigger_price=100.0,
-                        triggered_order=MarketOrderIntent(
+                        triggered_order=MarketOrderSpec(
                             quantity=1,
                             direction=Side.BUY
                         )
@@ -153,15 +147,15 @@ class TestBracketOrderIntent:
             )
     def test_bracket_order_intent_raises_if_take_profit_direction_same_as_entry(self):
         with pytest.raises(ValueError):
-            BracketOrderIntent(
-                entry=LimitOrderIntent(
+            BracketOrderSpec(
+                entry=LimitOrderSpec(
                     quantity=1,
                     direction=Side.BUY,
                     price=100.0
                 ),
                 stop_loss=None,
                 take_profit=[
-                    LimitOrderIntent(
+                    LimitOrderSpec(
                         quantity=1,
                         direction=Side.BUY,
                         price=100.0
@@ -170,23 +164,23 @@ class TestBracketOrderIntent:
             )
     def test_bracket_order_intent_raises_if_stop_loss_quantity_greater_than_entry(self):
         with pytest.raises(ValueError):
-            BracketOrderIntent(
-                entry=LimitOrderIntent(
+            BracketOrderSpec(
+                entry=LimitOrderSpec(
                     quantity=1,
                     direction=Side.BUY,
                     price=100.0
                 ),
                 stop_loss=[
-                    StopMarketOrderIntent(
+                    StopMarketOrderSpec(
                         trigger_price=100.0,
-                        triggered_order=MarketOrderIntent(
+                        triggered_order=MarketOrderSpec(
                             quantity=1,
                             direction=Side.SELL
                         )
                     ),
-                    StopMarketOrderIntent(
+                    StopMarketOrderSpec(
                         trigger_price=100.0,
-                        triggered_order=MarketOrderIntent(
+                        triggered_order=MarketOrderSpec(
                             quantity=1,
                             direction=Side.SELL
                         )
@@ -196,28 +190,28 @@ class TestBracketOrderIntent:
             )
     def test_bracket_order_intent_raises_if_take_profit_quantity_greater_than_entry(self):
         with pytest.raises(ValueError):
-            BracketOrderIntent(
-                entry=LimitOrderIntent(
+            BracketOrderSpec(
+                entry=LimitOrderSpec(
                     quantity=1,
                     direction=Side.BUY,
                     price=100.0
                 ),
                 stop_loss=[
-                    StopMarketOrderIntent(
+                    StopMarketOrderSpec(
                         trigger_price=100.0,
-                        triggered_order=MarketOrderIntent(
+                        triggered_order=MarketOrderSpec(
                             quantity=2,
                             direction=Side.SELL
                         )
                     ),
                 ],
                 take_profit=[
-                    LimitOrderIntent(
+                    LimitOrderSpec(
                         quantity=1,
                         direction=Side.SELL,
                         price=100.0
                     ),
-                    LimitOrderIntent(
+                    LimitOrderSpec(
                         quantity=1,
                         direction=Side.SELL,
                         price=100.0
@@ -225,20 +219,20 @@ class TestBracketOrderIntent:
                 ],
             )
     def test_bracket_order_intent_valid_case(self):
-        BracketOrderIntent(
-            entry=LimitOrderIntent(
+        BracketOrderSpec(
+            entry=LimitOrderSpec(
                 quantity=2,
                 direction=Side.BUY,
                 price=100.0
             ),
             stop_loss=None,
             take_profit=[
-                LimitOrderIntent(
+                LimitOrderSpec(
                     quantity=1,
                     direction=Side.SELL,
                     price=100.0
                 ),
-                LimitOrderIntent(
+                LimitOrderSpec(
                     quantity=1,
                     direction=Side.SELL,
                     price=100.0
