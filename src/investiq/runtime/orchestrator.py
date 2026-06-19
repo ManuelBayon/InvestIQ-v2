adf74d34e4a328198d95bd3b6c438d2bd35e0fcb
Manuel BAYON <manuel_bayon@outlook.fr>
2026-06-19 20:30:57 +0200

    Add OrderSubmitted handler to orchestrator

--- src/investiq/runtime/orchestrator.py
+++ src/investiq/runtime/orchestrator.py
@@ -1,6 +1,6 @@
 from collections.abc import Callable
 
-from investiq.events.events import CanonicalEvent, IntentGenerated, TickDataAvailable
+from investiq.events.events import CanonicalEvent, IntentGenerated, TickDataAvailable, OrderSubmitted
 from investiq.runtime.handlers.intent_generated_handler import IntentGeneratedHandler
 from investiq.runtime.handlers.tick_data_available_handler import TickDataAvailableHandler
 
@@ -23,11 +23,11 @@ class Orchestrator:
     def _on_tick_data_available(self, event: TickDataAvailable) -> IntentGenerated:
         return self._tick_data_available_handler.handle(event=event)
 
-    def _on_intent_generated(self, event: IntentGenerated) -> None:
+    def _on_intent_generated(self, event: IntentGenerated) -> OrderSubmitted:
         return self._intent_generated_handler.handle(event=event)
 
     def dispatch(self, event: CanonicalEvent) -> CanonicalEvent:
-        print(f"received event:{type(event).__name__}")
+        print(f"[DISPATCH] event:{type(event).__name__}")
         handler = self._dispatch_table[type(event)]
-        print(f"handler:{handler}")
+        print(f"[DISPATCH] handler:{handler}")
         return handler(event)
