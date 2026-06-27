20bb8710f5ae74887f56d5da4cdc0b8014583dcc
Manuel BAYON <manuel_bayon@outlook.fr>
2026-06-27 18:04:12 +0200

    refactor(IBKRClient): Remove access to ib_insycn, expose data and order API

--- src/investiq/adapters/ibkr_client.py
+++ src/investiq/adapters/ibkr_client.py
@@ -1,6 +1,6 @@
 from collections.abc import Callable
 
-from ib_insync import IB, Ticker
+from ib_insync import IB, Ticker, Contract, Order, Trade
 
 
 class IBKRClient:
@@ -13,28 +13,41 @@ class IBKRClient:
             host: str = "127.0.0.1",
             port: int = 7497,
             client_id: int = 1,
-
     ) -> None:
-        self._ib.connect(host, port, clientId=client_id)
-
-    def set_market_data_type(
-            self, data_type: int = 3
-    ) -> None:
-        """
-        data_type = 1 for live data
-        data_type = 3 for delayed data (default)
-        """
-        self._ib.reqMarketDataType(data_type)
+        self._ib.connect(
+            host=host,
+            port=port,
+            clientId=client_id
+        )
 
     def disconnect(self):
         self._ib.disconnect()
 
-    @property
-    def ib_client(self) -> IB:
-        return self._ib
+    def set_market_data_type(self, data_type: int = 3) -> None:
+        """
+        data_type = 1 for real-time
+        data_type = 3 for delayed data (by default)
+        """
+        self._ib.reqMarketDataType(marketDataType=data_type)
+
+    def run(self) -> None:
+        self._ib.run()
 
     def subscribe_pending_tickers(
             self,
-            handler: Callable[[set[Ticker]],None]
+            handler: Callable[[set[Ticker]], None]
     ) -> None:
         self._ib.pendingTickersEvent += handler
+
+    def request_market_data(self, contract: Contract) -> Ticker:
+        return self._ib.reqMktData(contract=contract)
+
+    def place_order(
+            self,
+            contract: Contract,
+            order: Order
+    ) -> Trade:
+        return self._ib.placeOrder(
+            contract=contract,
+            order=order
+        )
