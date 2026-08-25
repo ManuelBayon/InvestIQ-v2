a8d28121aecdd039ccef31164d6f87d8e66f96bc
Manuel BAYON <manuel_bayon@outlook.fr>
2026-08-25 16:28:39 +0200

    feat: add order types

---
+++ src/investiq/domain/order_types.py
@@ -0,0 +1,37 @@
+from abc import ABC
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Order(ABC):
+    ...
+
+
+@dataclass(frozen=True, slots=True)
+class MarketOrder(Order):
+    symbol: str
+    quantity: float
+
+
+@dataclass(frozen=True, slots=True)
+class LimitOrder(Order):
+    symbol: str
+    quantity: float
+    price: float
+
+
+@dataclass(frozen=True, slots=True)
+class StopLoss:
+    price: float
+
+
+@dataclass(frozen=True, slots=True)
+class TakeProfit:
+    price: float
+
+
+@dataclass(frozen=True, slots=True)
+class BracketOrder(Order):
+    entry: MarketOrder | LimitOrder
+    stop_loss: StopLoss
+    take_profit: LimitOrder
